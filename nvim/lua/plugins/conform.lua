f0dca08379b1eb9d6e1311266753d47f330d48b7
nnniv <nivroh2016@gmail.com>
2023-11-26 14:47:07 +0530

    add linting support

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,12 +1,13 @@
 return {
   "stevearc/conform.nvim",
-  config = function ()
+  config = function()
     require("conform").setup({
       formatters_by_ft = {
         lua = { "stylua" },
-        -- python = { "isort", "black" },
+        html = { "prettier" },
+        python = { "black" },
         javascript = { { "prettierd", "prettier" } },
       },
     })
-  end
+  end,
 }
