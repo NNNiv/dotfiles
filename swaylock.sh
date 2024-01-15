23f04867be6a42d39da7c4d59384aec0a25e3166
nnniv <nivroh2016@gmail.com>
2024-01-15 12:00:47 +0530

    add swaylock

---
+++ swaylock.sh
@@ -0,0 +1,29 @@
+swaylock \
+	--screenshots \
+  --font JetBrainsMonoExtraBold \
+	--clock \
+	--indicator \
+	--indicator-radius 100 \
+	--indicator-thickness 7 \
+	--effect-blur 7x5 \
+	--effect-vignette 0.5:0.5 \
+  \
+	--ring-color ab9df2 \
+  --ring-clear-color 78dce8 \
+  --ring-ver-color a9dc76 \
+  --ring-wrong-color ff6188 \
+  \
+	--key-hl-color 846fec \
+	--line-color 00000000 \
+  \
+	--inside-color 252838 \
+  --inside-clear-color 252838 \
+  --inside-ver-color 252838 \
+  --inside-wrong-color 252838 \
+  \
+  --text-color ab9df2 \
+  --text-clear-color 78dce8 \
+  --text-ver-color a9dc76 \
+  --text-wrong-color ff6188\
+  \
+	--separator-color 00000000 \
